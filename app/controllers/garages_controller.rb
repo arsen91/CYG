858d3fec4783e5e470bf361a88996291fa72789e
Arseni Prokharchyk <arsenpr2011@gmail.com>
2015-09-11 00:59:02 +0300

    start making form for adding garages

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,5 +1,24 @@
 class GaragesController < ApplicationController
+	before_action :all_garages, only: [:new, :search]
+	respond_to :html, :js
+
 	def search
-		
 	end
+
+	def new
+		@garage = Garage.new
+	end
+
+	def create
+		@garage = Garage.create(garage_params)
+	end
+
+	private
+		def all_garages
+			@garages = Garage.all
+		end
+
+		def garage_params
+      params.require(:garage).permit(:name, :address, :latitude, :longitude, :average_cost)
+    end
 end
