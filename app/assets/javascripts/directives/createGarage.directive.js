d959397c0524944ae35ffc68b2f580ba78a0a51a
Arseni Prokharchyk <asrenpr2011@gmail.com>
2016-02-22 09:20:57 +0300

    add directive to add marker

---
+++ app/assets/javascripts/directives/createGarage.directive.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    angular.module('CYG').directive('createGarageForm', ['mapFactory', function(mapFactory) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div class="modal-content"></div>',
+
+            link: function(scope, element) {
+                element.on("ajax:success", function(event, data) {
+                    mapFactory.addMarker(data.latitude, data.longitude);
+                });
+            }
+        };
+    }]);
+});
