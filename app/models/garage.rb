488169e5b3e252f671f8fe6a4faf7f53c8b2e093
Arseni Prokharchyk <asrenpr2011@gmail.com>
2016-02-24 01:24:49 +0300

    delete working time model

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,4 +1,6 @@
 class Garage < ActiveRecord::Base
     geocoded_by :address
     after_validation :geocode
+
+    has_one :working_time
 end
--- app/models/working_time.rb
+++
@@ -1,2 +0,0 @@
-class WorkingTime < ActiveRecord::Base
-end
